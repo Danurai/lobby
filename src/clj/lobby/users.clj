8aa99b3546ef6d10cb57be15bfe9ff5b3307e680
danurai <danurai74@gmail.com>
2020-09-10 23:17:42 +0100

    moar users

--- src/clj/lobby/users.clj
+++ src/clj/lobby/users.clj
@@ -9,6 +9,24 @@
 		:password (hash-bcrypt "user")
     :pin "1234" ;; only used by multi-factor
     :roles #{::user}}
+	"andy" {
+		:uid 1002
+		:username "andy"
+		:password (hash-bcrypt "user")
+    :pin "1234" ;; only used by multi-factor
+    :roles #{::user}}
+	"matt" {
+		:uid 1003
+		:username "matt"
+		:password (hash-bcrypt "user")
+    :pin "1234" ;; only used by multi-factor
+    :roles #{::user}}
+	"julio" {
+		:uid 1004
+		:username "julio"
+		:password (hash-bcrypt "user")
+    :pin "1234" ;; only used by multi-factor
+    :roles #{::user}}
   "root" {
 		:uid 1000
 		:username "root"
