9bc44255e826f6797bda16dcaa030f3671a257f9
Danurai <bikerdan13@yahoo.co.uk>
2020-10-13 22:23:32 +0100

    piggieback fix

--- project.clj
+++ project.clj
@@ -85,11 +85,11 @@
                      [expectations "2.2.0-rc3"]
                      [binaryage/devtools "0.9.4"]
                      [figwheel-sidecar "0.5.14"]
-                     [com.cemerick/piggieback "0.2.2"]]
+                     [cider/piggieback "0.4.0"]]
       ;; need to add dev source path here to get user.clj loaded
       :source-paths ["src" "dev"]
       ;; for CIDER
       ;; :plugins [[cider/cider-nrepl "0.12.0"]]
-      :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+      :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
       ;; need to add the compliled assets to the :clean-targets
       :clean-targets ^{:protect false} ["resources/public/js/compiled" :target-path]}})
