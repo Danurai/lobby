13595f984814458bcbe5209d78d1d974d541d883
Danurai <Danurai@users.noreply.github.com>
2020-11-12 17:48:47 +0000

    Add files via upload

--- test/lobby/datest.clj
+++ test/lobby/datest.clj
@@ -16,7 +16,7 @@
   (let [gid (newgamegid "Death Angel")]
     (-> @model/appstate :games gid :game)))
     
-(expect {:status :setup :teamlimit 3 :players {"p1" {:public {:teams []}}}}
+(expect {:status :setup :teamlimit 3 :players {"p1" {:public {:teams []}}} :maxsupport 12}
   (let [gid (newgamegid "Death Angel")]
     (-> (model/startgame! gid)
         :games 
@@ -68,7 +68,7 @@
         (damodel/pickteam "p1" :purple)
         :teams))))
           
-; Release Team
+; Release / drop Team
 (expect 0
   (let [gid (newgamegid "Death Angel")]
     (count (filter (fn [[k v]] (= (:cmdr v) "p1"))
@@ -78,3 +78,42 @@
         (damodel/pickteam "p1" :green )
         :teams)))))
   
+; Start 1p game
+(expect true
+  (-> onepgm (damodel/parseaction :start "p1") some?))
+  
+; 1. Setup decks
+(expect true
+  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+    :event some?))
+(expect 29
+  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+    (damodel/obfuscate "p1") :events))
+(expect 20
+  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+     (damodel/obfuscate "p1") :gdeck))
+; 3. Setup location 'deck'
+(expect [:2 :3 :4]
+  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+    :path))
+(expect {:maj 2 :min 1}
+  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+    :spawns))
+; 2. Setup starting location
+(expect {:id :void3 :stage :0 :name "Void Lock"}
+  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+    :location (select-keys [:id :stage :name])))
+; 4. Choose combat teams
+; 5. Setup formation
+(expect 6
+  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+    :formation count))
+; 6. Place support tokens
+;; 7. Setup terrain and blips
+;(expect {:top 2 :bottom 2}
+;  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+;    :terrain frequencies))
+;; 8. Spawn starting genestealers
+;(expect true
+;  (-> onepgm (damodel/parseaction {:action :start} "p1") 
+;    :enemies))
