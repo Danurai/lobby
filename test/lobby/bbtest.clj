dd0fd49adb68c1d92e9644217caf485db1cb3f6a
Danurai <Danurai@users.noreply.github.com>
2021-05-19 17:32:51 +0100

    Add files via upload

--- test/lobby/bbtest.clj
+++ test/lobby/bbtest.clj
@@ -5,52 +5,125 @@
     [lobby.bbmodel :as bbmodel]
     [lobby.lobbytest :refer :all]))
 
-(def twoplyrgamewithai   
-  (let [gs (->> (bbmodel/setup ["p1" "AI1234"]))
-        p1team (if (= (-> gs :players (get "AI1234") :team) "Orc") "Human" "Orc")]
-    (bbmodel/parseaction gs {:action :chooseteam :team p1team} "p1") ))
+(def twoplyrgame 
+  (-> ["p1" "p2"]
+      bbmodel/setup 
+      (bbmodel/parseaction {:action :chooseteam :team "Human"} "p1")
+      (bbmodel/parseaction {:action :chooseteam :team "Orc"} "p2"))) 
 
-; create 2p game
-(expect ["p1" "AI1234"]
-  (->> (bbmodel/setup ["p1" "AI1234"])
-       :players
-       keys))
+; Create Game, select teams.
+;; two players
+(expect ["p1" "p2"] (-> twoplyrgame :players keys))
+;; two teams
+(expect 2 (->> twoplyrgame :players vals (map :team) count))
+;; Turn = 0
+(expect 0 (-> twoplyrgame :turn))
+;; remove all other team info
+(expect nil (-> twoplyrgame :teams))
+;; Freebooters
+;;
+;; Draw highlights !TODO 4x for 2p game!
+(expect #(= (-> % :turnorder count) (-> :highlights :public count) ) (twoplyrgame))
+;; 6 private players and 6 secret players each
+(expect 6 (-> twoplyrgame :players (get "p1") :team :private count))
+(expect 6 (-> twoplyrgame :players (get "p1") :team :secret count))
+(expect 6 (-> twoplyrgame :players (get "p2") :team :private count))
+(expect 6 (-> twoplyrgame :players (get "p2") :team :secret count))
 
-(expect 2
-  (let [gs (->> (bbmodel/setup ["p1" "AI1234"]))
-        p1team (if (= (-> gs :players (get "AI1234") :team) "Orcs") "Human" "Orcs")]
-    (->> 
-         (bbmodel/choose-team gs p1team "p1")  
-         :players vals
-         (map :team)
-         count)))
+;; set team alliance
+(expect ["Human" "OWA"]
+  (let [gs twoplyrgame
+        team (-> gs :players (get "p1") :team)]
+    [ (:team team) (:alliance team) ]))
+;; set team alliance
+(expect ["Orc" "CWC"]
+  (let [gs twoplyrgame
+        team (-> gs :players (get "p2") :team)]
+    [ (:team team) (:alliance team) ]))
 
-(expect 2
-  (->> twoplyrgamewithai
-      :players vals
-      (map :team)
-      count))
+; :matchup status on players turn
+(expect :matchup
+  (let [gs twoplyrgame
+        p1 (-> gs :turnorder first)]
+    (-> gs :players (get p1) :state)))
 
-; Start Game. Turn = 0
-; remove all team info
-;; create freebooter deck
-; draw 2 highlights 
-; each player draws 6
+(expect :matchup
+  (let [gs twoplyrgame
+        p1 (-> gs :turnorder first)
+        p2 (-> gs :turnorder second)
+        h1 (-> gs :highlights :public first)
+        p1 (-> gs :players (get p1) :team :private first)]
+    (-> gs 
+        (bbmodel/commit-player p1 (:id p1) (:id h1) :a)
+        :players (get p2) :state)))
 
-(expect 0 (-> twoplyrgamewithai :turn))
-(expect nil (-> twoplyrgamewithai :teams))
-(expect #(= (-> % :turnorder count) (-> :highlights :public count) ) (twoplyrgamewithai))
-(expect 6 (-> twoplyrgamewithai :players (get "p1") :team :private count))
-(expect 6 (-> twoplyrgamewithai :players (get "p1") :team :secret count))
-(expect 6 (-> twoplyrgamewithai :players (get "AI1234") :team :private count))
-(expect 6 (-> twoplyrgamewithai :players (get "AI1234") :team :secret count))
+; Start Game, commit player to matchup - 1 player in zone
+(expect 1
+  (let [gs (assoc twoplyrgame :turnorder ["p1" "p2"])
+        h1 (-> gs :highlights :public first)
+        p1 (-> gs :players (get "p1") :team :private first)]
+    (-> gs 
+        (bbmodel/parseaction {:action :commitplayer :plid (:id p1) :hlid (:id h1) :zone :a} "p1")
+        :highlights :public first :zone :a count)))
+; Start Game, commit player to matchup - 5 players remaining
+(expect 5
+  (let [gs (assoc twoplyrgame :turnorder ["p1" "p2"])
+        h1 (-> gs :highlights :public first)
+        p1 (-> gs :players (get "p1") :team :private first)]
+    (-> gs 
+        (bbmodel/parseaction {:action :commitplayer :plid (:id p1) :hlid (:id h1) :zone :a} "p1")
+        :players (get "p1") :team :private count)))
+
+
+; Pass
+(expect true
+  (let [gs twoplyrgame
+        p1 (-> gs :turnorder first)]
+    (-> gs
+        (bbmodel/parseaction {:action :pass} p1)
+        :players (get p1) :passed?)))
+
+(expect :scoreboard
+  (let [gs twoplyrgame
+        p1 (-> gs :turnorder first)
+        p2 (-> gs :turnorder last)
+        hl (-> gs :highlights :public first)]
+    (-> 
+      (reduce 
+        (fn [state plyr]
+          (-> state
+              (bbmodel/parseaction {:action :commitplayer :plid (-> state :players (get p1) :team :private first :id) :hlid (:id hl) :zone :a} p1)
+              (bbmodel/parseaction {:action :commitplayer :plid (-> state :players (get p2) :team :private first :id) :hlid (:id hl) :zone :b} p2)))
+        gs
+        (range 6))
+      :status))) 
+
+; Commit-Player
+
+; AI Tests
+;(def twoplyrgamewithai   
+;  (let [gs (->> (bbmodel/setup ["p1" "AI1234"]))
+;        p1team (if (= (-> gs :players (get "AI1234") :team) "Orc") "Human" "Orc")]
+;    (bbmodel/parseaction gs {:action :chooseteam :team p1team} "p1") ))
+
+; AI-CHOOSE-TEAM
+(expect "Orc"
+  (let [teststate {
+              :teams [ {:team "Human"} {:team "Orc"} ]
+              :players {
+                "AI1" {:team "Human"}
+                "AI2" {} } }]
+    (bbmodel/ai-choose-team teststate)))
+
+; AI-COMMIT-PLAYER [highlightid playerid zone]
+;; identify zones
+
 
 (expect true
-  (let [gs (->> (bbmodel/setup ["p1" "AI1234"]))
-        p1team (if (= (-> gs :players (get "AI1234") :team) "Orc") "Human" "Orc")]
-    (=
-      (-> (bbmodel/parseaction gs {:action :chooseteam :team p1team} "p1") 
-        :players 
-        (get "p1")
-        :team :alliance)
-      (->> gs :teams (filter #(= (:team %) p1team)) first :alliance))))
+  (let [teststate twoplyrgame 
+        players (map :id (-> teststate :players (get "p1") :team :private)) 
+        highlights (->> teststate :highlights (map) :id)
+        ai-commit (-> teststate (bbmodel/ai-commit-player "p1"))]
+      (and (-> ai-commit first number?)
+           (-> ai-commit second number?)
+           (or (-> ai-commit last (= :a))(-> ai-commit last (= :b))))))
