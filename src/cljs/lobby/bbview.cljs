ffb60854d6ddfd8730ce2f0672c496967ebaf1ea
Danurai <Danurai@users.noreply.github.com>
2021-05-28 16:43:21 +0100

    Add files via upload

--- src/cljs/lobby/bbview.cljs
+++ src/cljs/lobby/bbview.cljs
@@ -14,7 +14,7 @@
 (defn- chat [ ]
   [:div.chat
     [:div.border.rounded.p-1.chatbox   ;{:style {:min-height (* 45 (-> @ra-app :settings :cardsize :scale))}}
-      (for [msg (-> @gm :state :chat) :let [{:keys [msg uname timestamp]} msg]]
+      (for [msg (-> @gm :state :chat reverse) :let [{:keys [msg uname timestamp]} msg]]
         [:div {:key (gensym) :style {:word-wrap "break-word"}}
           ;[:span.mr-1 (model/timeformat timestamp)]
           [:b.text-primary.mr-1 uname]
@@ -42,11 +42,12 @@
 (defn- highlight-zone [ hl zone ]
   (let [tzones (get-team-zone-map  hl) pteam (-> @gm :state :players (get @uname) :team :team)]
     [:div.col-4 
-      (if (and (= (-> @gm :state :activeplyr) @uname)
+      (if (and (= :matchup (-> @gm :state :players (get @uname) :state))
+               (= (-> @gm :state :activeplyr) @uname)
                (->> @gm :state :players (get @uname) :passed? ((complement true?))))
         (if (or (empty? tzones)
                 (-> tzones (get pteam) (= zone)) 
-                (and (-> tzones (get pteam) nil?) (-> hl :zone zone empty?) )) 
+                (and (-> tzones (get pteam) nil?) (-> hl :zone zone empty?) ))
           (if (-> @bb-app :matchupcommit? some?)
               (if (and (-> @bb-app :matchupcommit? :id (= (:id hl))) (-> @bb-app :matchupcommit? :zone (= zone))) [:button.btn.btn.sm.w-100.btn-warning "Select a player to commit (Cancel)"])
               [:button.btn.btn-sm.w-100.btn-light {:on-click (fn [e] (.stopPropagation e) (swap! bb-app assoc :matchupcommit? {:id (:id hl) :zone zone}))} "Commit"])))
@@ -58,7 +59,9 @@
               :style {:background-image (str "url('" url "')" ) }
               :on-mouse-move #(setbigview url) :on-mouse-out #(setbigview nil) :on-click #(showbigview)} 
             [:img.team-marker {:src (str "/img/bb/images/Marker " (:team p) ".png")}]
-            [:span.label (:position p)]
+            [:span.label 
+              [:span.mr-2 (:position p)]
+              (if (-> hl :ballcarrier (= {:zone zone :id (:id p)})) [:b "Ball"])]
             [:span.spp (-> p :spp (get (if (:prone? p) 1 0)))]
             ]))]))
 
@@ -71,10 +74,10 @@
           [:div.col-4
             [:div.row.highlight
               [:div.d-flex.justify-content-around.scoreboard 
-                [:div (->> hl :zone :a (map #(+ (if (:hasball? %) 2 0) (if (:prone? %) (-> % :spp last) (-> % :spp first)))) (reduce +) )]
-                [:div (->> hl :zone :b (map #(+ (if (:hasball? %) 2 0) (if (:prone? %) (-> % :spp last) (-> % :spp first)))) (reduce +) )]]
+                [:div (->> hl :zone :a (map #(+ (if (= {:zone :a :id (:id %)} (:ballcarrier hl)) 2 0) (if (:prone? %) (-> % :spp last) (-> % :spp first)))) (reduce +) )]
+                [:div (->> hl :zone :b (map #(+ (if (= {:zone :b :id (:id %)} (:ballcarrier hl)) 2 0) (if (:prone? %) (-> % :spp last) (-> % :spp first)))) (reduce +) )]]
               [:img.img-fluid {:on-mouse-over #(swap! bb-app assoc :bigview src) :on-click #(swap! bb-app assoc :showbigview? true) :src src } ]
-              [:img.football {:src "img/bb/images/Football.png"}]]]
+              (if (-> hl :ballcarrier nil?) [:img.football {:src "img/bb/images/Football.png"}])]]
           (highlight-zone hl :b)]]))])
 
 
@@ -110,10 +113,60 @@
               (teamplyrcard p)]))]]
       [:div.col-2 (chat)]]))
 
+(defn response [ state resp s ]
+  (case (:id resp)
+    :discard 
+      [:div
+        [:h5.text-center "Discard One" ]
+        [:div.d-flex.justify-content-center
+          (for [p (-> state :players (get @uname) :team :private)]
+            [:div.playercard.selectable {:key (gensym)}
+              [:img.img-fluid {:src (str "/img/bb/images/" (:team p) " - " (:position p) ".png") :on-click #(comms/ra-send! {:action :response :id (:id p)})}]])]]
+    :tackle-target 
+      (let [committed (-> state :players (get @uname) :committed) hlid (:hlid committed) zone (if (= :a (:zone committed)) :b :a)]
+        [:div 
+          [:h5.text-center (:msg resp)]
+          (for [p (->> state :highlights :public (filter #(= (:id %) hlid)) first :zone zone)]
+            [:div.playercard.selectable {:key (gensym)}
+            [:img.img-fluid {:src (str "/img/bb/images/" (:team p) " - " (:position p) ".png") :on-click #(comms/ra-send! {:action :response :id (:id p)})}]])
+        ])
+    [:div [:button.btn.btn-success {:on-click #(comms/ra-send! {:action :response})} (:msg resp)]]))
+
+(defn abilities [ state ]
+  (let [plyr   (-> state :players (get @uname) :committed)
+        src    (str "/img/bb/images/" (:team plyr) " - " (:position plyr) ".png") 
+        active (-> plyr :skills (get (:activeskill plyr)))]
+    [:div.abilities.d-flex
+      [:div.playercard 
+        [:img.img-fluid {
+          :src src 
+          :on-mouse-move #(setbigview src) 
+          :on-mouse-out #(setbigview nil) 
+          :on-click #(showbigview) }]]
+      [:div.d-flex
+        (doall (map-indexed 
+          (fn [id s] 
+            [:div {:key (gensym)}
+              (if (not= id (:activeskill plyr))
+                [:div.h4.mx-2 {:class (if (<= id (:activeskill plyr)) "text-light") } s]
+                (if-let [resp (-> state :players (get @uname) :response)]
+                  (response state resp s)
+                  [:div
+                    [:div.h2.text-center active]
+                    [:div 
+                      (if (not= :cheat active)
+                        [:button.btn.btn-warning.mr-2 {:on-click #(if (js/confirm "Are you sure you want to skip using this skill?") (comms/ra-send! {:action :skill-pass}))} "Skip"])
+                      [:button.btn.btn-success {:on-click #(comms/ra-send! {:action :skill-use :skill active})} s]]]))])
+          (:skills plyr)))]
+      [:div.ml-auto (chat)]
+    ]))
+
 (defn gameview [ state ]
   [:div.col
     (highlights state)
-    (hand state @uname)]
+    (if (-> state :players (get @uname) :state (= :skills))
+      (abilities state)
+      (hand state @uname))]
   )
 
 (defn- teamcard [ team ]
@@ -137,27 +190,17 @@
               [:div.text-center [:b (if (some? pteam) pteam (if (and isap? (= ap p)) "Choosing Team" "Waiting for Coach Choices"))]]]])]
       [:div.row-fluid [:div.h5.text-center "Starting Rosters"]]
       [:div.row-fluid
-        [:div.d-flex
-          [:div.list-group.list-group-horizontal.mx-auto
+        [:div.d-flex.mb-2.justify-content-center
             (for [ t (->> state :teams (map :team) distinct sort)
                     :let [active? (= t team)
                           alliance (->> state :teams (filter #(= (:team %) t)) first :alliance)]]
-              [:li.list-group-item.alliance-bg {
-                  :key (gensym) 
-                  :class (if active? "active") 
-                  :on-click #(swap! bb-app assoc :team t)
-                  :style {:background-image (str "url('/img/bb/images/" alliance " Back ls.png')")}} 
-                [:div.label t]])]]
+              [:button.btn.alliance {:key (gensym) :class (if active? "active") :on-click #(swap! bb-app assoc :team t)} [:img.img-fluid {:src (str "/img/bb/images/Marker " t ".png")}]])]
+        (if (and isap? (->> team (contains? chosenteams) false?))
+            [:div.d-flex.justify-content-center.mb-2 [:button.btn.btn-light {:on-click #(comms/ra-send! {:action :chooseteam :team team})} "Field " team " team."]])  ;[:img.ml-1 {:src (str "/img/bb/images/Marker " team ".png")}]
         [:div.d-flex.justify-content-center
           (doall (for [ tq (->> state :teams (filter #(= (:team %) team)) (sort-by :id) (map :position) frequencies) ]
             [:div.p-2 {:key (gensym)}
-              (teamplyrcard {:position (first tq) :qty (last tq) :team team :disabled? (contains? chosenteams team)})]))
-          (if (and isap? (->> team (contains? chosenteams) false?))
-              [:div.ml-2
-                [:div.text-center [:b "Choose team" ]]
-                [:button.btn {:on-click #(comms/ra-send! {:action :chooseteam :team team})} [:img.ml-1 {:src (str "/img/bb/images/Marker " team ".png")}]]])
-              ]]
-    ]))
+              (teamplyrcard {:position (first tq) :qty (last tq) :team team :disabled? (contains? chosenteams team)})]))]]]))
 
 ; MAIN
 
@@ -178,15 +221,16 @@
       (case (:status state)
         :setup (setupview state)
         (gameview state))
-      (if true? false
-        [:div
+      (if (true? false)
+        [:div.bg-light
+          [:div (-> state :players (get @uname) :response str)]
           [:div (str @bb-app)]
-          [:div.bg-light (-> state (dissoc :players :chat :highlights) str)]
-          [:div.bg-light (-> state :highlights :public first str)]
-          [:div.bg-light (-> state :highlights :public second str)]
-          [:div.bg-light (-> state :players first first str)]
-          [:div.bg-light (-> state :players first second str)]
-          [:div.bg-light (-> state :players second first str)]
-          [:div.bg-light (-> state :players second second str)]]
+          [:div (-> state (dissoc :players :chat :highlights) str)]
+          [:div (-> state :highlights :public first str)]
+          [:div (-> state :highlights :public second str)]
+          [:div (-> state :players first first str)]
+          [:div (-> state :players first second str)]
+          [:div (-> state :players second first str)]
+          [:div (-> state :players second second str)]]
       )
     ]))
